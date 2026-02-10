aad96426fe4e37149bff1a30403f8af81238e208
alejandropf3 <alejandropefa31@gmail.com>
2026-02-10 10:13:48 -0500

    limpieza total y aplicacion de git ignore

---
+++ proyecto_economic/src/main/java/com/backend/proyecto_economic/config/configDB.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.proyecto_economic.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Propietario
+ */
+public class configDB {
+    private static String URL = "jdbc:mysql://localhost:3306/economic";
+    private static String USER = "root";
+    private static String PASSWD = "#Aprendiz2024";
+
+    public static Connection iniciarConexionDB() {
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Conexion establecida con exito.");
+            return connection;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
