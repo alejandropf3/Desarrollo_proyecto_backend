29656d504a4f69f28667cb4ab08b07cc23509d21
alejandropf3 <alejandropefa31@gmail.com>
2026-02-10 09:26:00 -0500

    Se Creo la estructura basica del proyecto( se creo la conexion con la base de datos, se configuro el pom y se crearon las carpetas config,dao,controllers y models)

---
+++ proyecto_economic/src/main/java/com/backend/proyecto_economic/Proyecto_economic.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.backend.proyecto_economic;
+import com.backend.proyecto_economic.config.configDB;
+/**
+ *
+ * @author Propietario
+ */
+public class Proyecto_economic {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo :)");
+        configDB.iniciarConexionDB();
+    }
+}
